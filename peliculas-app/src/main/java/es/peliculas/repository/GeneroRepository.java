813a2bda58ba629daedb8441ae18fb0835ebc6ed
marioRACampusFP <mario.rodriguezarevalillo24@campusfp.es>
2026-01-16 12:56:19 +0100

    Delete peliculas-app directory

--- peliculas-app/src/main/java/es/peliculas/repository/GeneroRepository.java
+++
@@ -1,7 +0,0 @@
-package es.peliculas.repository;
-
-import es.peliculas.entity.Genero;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GeneroRepository extends JpaRepository<Genero, Long> {
-}
--- peliculas-app/src/main/java/es/peliculas/repository/PeliculaRepository.java
+++
@@ -1,7 +0,0 @@
-package es.peliculas.repository;
-
-import es.peliculas.entity.Pelicula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PeliculaRepository extends JpaRepository<Pelicula, Long> {
-}
